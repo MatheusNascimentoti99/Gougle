946f7bc81441879a0a81a85c6049af7033866abc
Usuário 01 <Usurio 01@DESKTOP-7DREGPM>
2018-06-30 02:37:00 -0300

    testes

--- test/util/CrescenteTest.java
+++ test/util/CrescenteTest.java
@@ -5,6 +5,8 @@
  */
 package util;
 
+import model.Pagina;
+import model.Palavra;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -12,33 +14,47 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author Matheus Nascimento
- */
+ /**
+  * Classe de Testes da classe Crescente.
+  * 
+ * @author Matheus Nascimento e Elvis Serafim
+ * @since Jul 2018
+ * @version 1.0
+ **/
 public class CrescenteTest {
     
+    private Crescente crescente;
+    private Palavra p, p2;
+    private Pagina pag, pag2;
+   
     public CrescenteTest() {
+        
+        crescente = new Crescente();
+         p = new Palavra("palavra1", new Pagina("page1", 1111));
+        p2 = new Palavra("palavra2", new Pagina("page2", 1113));
+        pag = new Pagina("pagina", 1122);
+        pag2 = new Pagina("pagina", 1322);        
     }
     
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
-
     @Test
     public void testCompare() {
+        p.setSearch(2);
+        p2.setSearch(3);
+        
+        assertEquals(1, crescente.compare(p, p2));
+        p.setSearch(3);
+        assertEquals(0, crescente.compare(p, p2));
+        p.setSearch(4);
+        assertEquals(-1, crescente.compare(p, p2));
+        
+        pag.setVisit(3);
+        pag2.setVisit(2);
+        
+        assertEquals(-1, crescente.compare(pag, pag2));
+        pag2.setVisit(3);
+        assertEquals(0, crescente.compare(pag, pag2));
+        pag2.setVisit(4);
+        assertEquals(1, crescente.compare(pag, pag2));
     }
     
 }
