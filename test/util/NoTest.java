18b5891ba8ccd0a69874a9789f9c0daec2924b08
MatheusNascimentoti99 <32804625+MatheusNascimentoti99@users.noreply.github.com>
2018-06-30 03:10:26 -0300

    Criando classes de teste

---
+++ test/util/NoTest.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Matheus Nascimento
+ */
+public class NoTest {
+    
+    public NoTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testGetDate() {
+    }
+
+    @Test
+    public void testSetDate() {
+    }
+
+    @Test
+    public void testGetBalanceamento() {
+    }
+
+    @Test
+    public void testSetBalanceamento() {
+    }
+
+    @Test
+    public void testGetPai() {
+    }
+
+    @Test
+    public void testSetPai() {
+    }
+
+    @Test
+    public void testGetRight() {
+    }
+
+    @Test
+    public void testSetRight() {
+    }
+
+    @Test
+    public void testGetLeft() {
+    }
+
+    @Test
+    public void testSetLeft() {
+    }
+    
+}
